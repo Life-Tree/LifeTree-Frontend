2231f807ca3e466466efb10355358735502158fe
Maria Fernanda Durango <mdurangoh@unicartagena.edu.co>
2021-10-10 00:21:12 -0500

    -Esta vaina esta mocha pero aja toco mergiar(Avace en modal -especies)

--- src/app/pages/especies-modal/especies-modal.page.ts
+++ src/app/pages/especies-modal/especies-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-especies-modal',
@@ -7,14 +8,20 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./especies-modal.page.scss'],
 })
 export class EspeciesModalPage implements OnInit {
-
+   nombreE: string;
+   familiaE: string;
+   descripciónE: string;
  
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController, public navParamsNombre : NavParams,
+     public navParamsFamilia : NavParams, public navParamsDescripcion : NavParams) { }
 
   ngOnInit() {
+    this.nombreE = this.navParamsNombre.get('nombre'); 
+    this.familiaE = this.navParamsFamilia.get('familia');
+    this.descripciónE = this.navParamsDescripcion.get('descripcion'); 
   }
 
-  dismiss() {
+  async dismiss() {
     // using the injected ModalController this page
     // can "dismiss" itself and optionally pass back data
     this.modalController.dismiss({
